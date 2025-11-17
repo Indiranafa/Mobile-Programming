aa0e9eeab01a5b76c0798ce67a24d8b21d063e9e
Indira Nafa <indiranafa24@gmail.com>
2025-11-18 01:47:04 +0700

    add: Praktikum 2 dan 3

--- project-flutter/app/week10/master_plan/lib/provider/plan_provider.dart
+++ project-flutter/app/week10/master_plan/lib/provider/plan_provider.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import '../models/data_layer.dart';
 
-class PlanProvider extends InheritedNotifier<ValueNotifier<Plan>> {
-  const PlanProvider({super.key, required ValueNotifier<Plan> notifier, required super.child})
+class PlanProvider extends InheritedNotifier<ValueNotifier<List<Plan>>> {
+  const PlanProvider({super.key, required ValueNotifier<List<Plan>> notifier, required super.child})
       : super(notifier: notifier);
 
-  static ValueNotifier<Plan> of(BuildContext context) {
-   return context.dependOnInheritedWidgetOfExactType<PlanProvider>()!.notifier!;
+  static ValueNotifier<List<Plan>> of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<PlanProvider>()!.notifier!;
   }
 }
